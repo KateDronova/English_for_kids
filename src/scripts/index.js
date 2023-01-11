eb652fb5687ab1a51ed3778faf1cf108bfdfe524
Ekaterina Dronova <katedronova96@gmail.com>
2023-01-11 17:49:46 +0300

    fix: adjust routing logic

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,58 +1,56 @@
-
-import '../css/normalize.css';
-import '../css/style.css';
-import setRoutes from './app.js';
+import "../css/normalize.css";
+import "../css/style.css";
+import { menuItems } from "./router.js";
+import setRoutes from "./app.js";
 setRoutes();
-import { setSection, fillInTheContent, addFlipEffect } from './section.js';
 
 ///// Mobile menu
-const burger = document.querySelector('.burger');
-const menu = document.querySelector('.menu');
-const menuItems = document.querySelectorAll('nav li');
-const cover = document.querySelector('.cover');
-const burgerDown = document.getElementById('down');
-const burgerSalad = document.getElementById('salad');
-const burgerCutlet = document.getElementById('cutlet');
-const burgerTomato = document.getElementById('tomato');
+const burger = document.querySelector(".burger");
+const menu = document.querySelector(".menu");
+const cover = document.querySelector(".cover");
+const burgerDown = document.getElementById("down");
+const burgerSalad = document.getElementById("salad");
+const burgerCutlet = document.getElementById("cutlet");
+const burgerTomato = document.getElementById("tomato");
 
 function closeTheMenu() {
   cover.hidden = true;
-  burgerDown.classList.toggle('unpack1');
-  burgerSalad.classList.toggle('unpack2');
-  burgerCutlet.classList.toggle('unpack3');
-  burgerTomato.classList.toggle('unpack4');
-  menu.classList.toggle('move');
-  menu.addEventListener('transitionend', function() {
-      menu.style.display = 'none';
-      menu.hidden = true;
+  burgerDown.classList.toggle("unpack1");
+  burgerSalad.classList.toggle("unpack2");
+  burgerCutlet.classList.toggle("unpack3");
+  burgerTomato.classList.toggle("unpack4");
+  menu.classList.toggle("move");
+  menu.addEventListener("transitionend", function () {
+    menu.style.display = "none";
+    menu.hidden = true;
   });
 }
 
-burger.addEventListener('click', function() {
-    if (menu.hidden) {
-        menu.hidden = false;
-        cover.hidden = false;
-        menu.style.display = 'block';
-        menu.classList.toggle('move');
-        burgerDown.classList.toggle('unpack1');
-        burgerSalad.classList.toggle('unpack2');
-        burgerCutlet.classList.toggle('unpack3');
-        burgerTomato.classList.toggle('unpack4');
-    } else {
-        closeTheMenu();
-    }
+burger.addEventListener("click", function () {
+  if (menu.hidden) {
+    menu.hidden = false;
+    cover.hidden = false;
+    menu.style.display = "block";
+    menu.classList.toggle("move");
+    burgerDown.classList.toggle("unpack1");
+    burgerSalad.classList.toggle("unpack2");
+    burgerCutlet.classList.toggle("unpack3");
+    burgerTomato.classList.toggle("unpack4");
+  } else {
+    closeTheMenu();
+  }
 });
-cover.addEventListener('click', closeTheMenu);
+cover.addEventListener("click", closeTheMenu);
 
-for (let item of menuItems) {
-  item.addEventListener('click', closeTheMenu);
-}
+menuItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    menuItems.forEach((item) => item.classList.remove("active"));
+    item.classList.add("active");
+  });
+  item.addEventListener("click", closeTheMenu);
+});
 
 ///// Toggle
-document.getElementById("toggle").addEventListener("change", function() {
-    this.setAttribute("aria-checked", this.checked);
-  });
-
-///// Href
-// const hrefs = document.querySelectorAll('a');
-// hrefs.forEach((href) => href.onclick(setSection()));
+document.getElementById("toggle").addEventListener("change", function () {
+  this.setAttribute("aria-checked", this.checked);
+});
--- src/scripts/section.js
+++ src/scripts/section.js
@@ -1,84 +1,73 @@
 import cardsInfoList from "./cards.js";
 
 ///// Fill in the section for Training mode
+function fillInTheContent(j) {
+  const subheader = document.getElementById("subHead");
+  const cardImages = document.querySelectorAll(".front img");
+  const cardNames = document.querySelectorAll(".front h3");
+  const cardSounds = document.querySelectorAll(".front audio");
+  const cardBackImages = document.querySelectorAll(".back img");
+  const cardBackNames = document.querySelectorAll(".back h3");
 
-const j = 1;
-const subheader = document.getElementById('subheader');
+  subheader.textContent = cardsInfoList[j][0];
 
-// const cards = document.querySelectorAll('.card');
-const cardImages = document.querySelectorAll('.front img');
-const cardNames = document.querySelectorAll('.front h3');
-const cardSounds = document.querySelectorAll('.front audio');
-// const cardBacks = document.querySelectorAll('.back');
-const cardBackImages = document.querySelectorAll('.back img');
-const cardBackNames = document.querySelectorAll('.back h3');
-
-function fillInTheContent() {
-  let i = 0;
-  // console.log(cardsInfoList);
-  // debugger;
-  subheader.textContent = cardsInfoList[j][i];
-
+  let i = 1;
   for (let cardImg of cardImages) {
-    cardImg.setAttribute('src', cardsInfoList[j][i].image);
+    cardImg.setAttribute("src", cardsInfoList[j][i].image);
     i++;
   }
 
-  i = 0;
+  i = 1;
   for (let cardName of cardNames) {
     cardName.innerHTML = cardsInfoList[j][i].word;
     i++;
   }
 
-  i = 0;
+  i = 1;
   for (let cardBackImg of cardBackImages) {
-    cardBackImg.setAttribute('src', cardsInfoList[j][i].image);
+    cardBackImg.setAttribute("src", cardsInfoList[j][i].image);
     i++;
   }
 
-  i = 0;
+  i = 1;
   for (let cardBackName of cardBackNames) {
     cardBackName.innerHTML = cardsInfoList[j][i].translation;
     i++;
   }
 
-  i = 0;
+  i = 1;
   for (let cardSound of cardSounds) {
-    cardSound.setAttribute('src', cardsInfoList[j][i].audioSrc);
+    cardSound.setAttribute("src", cardsInfoList[j][i].audioSrc);
     i++;
-    cardSound.closest('div').addEventListener('click', function (event) {
-      if (!event.target.closest('button')) {
+    cardSound.closest("div").addEventListener("click", function (event) {
+      if (!event.target.closest("button")) {
         cardSound.play();
       }
     });
   }
 }
 
-
-const flipButtons = document.querySelectorAll('div button');
-
 function addFlipEffect() {
+  const flipButtons = document.querySelectorAll("div button");
+
   for (let flipButton of flipButtons) {
-    flipButton.addEventListener('click', function () {
-      flipButton.closest('div').classList.add('clicked');
-      flipButton.closest('div').nextElementSibling.classList.remove('clicked');
-    })
-    flipButton.closest('div').nextElementSibling.addEventListener('mouseout', function () {
-      if (!flipButton.closest('div').nextElementSibling.hidden) {
-        flipButton.closest('div').hidden = false;
-        flipButton.closest('div').classList.remove('clicked');
-        flipButton.closest('div').nextElementSibling.classList.add('clicked');
-      }
+    flipButton.addEventListener("click", function () {
+      flipButton.closest("div").classList.add("clicked");
+      flipButton.closest("div").nextElementSibling.classList.remove("clicked");
     });
+    flipButton
+      .closest("div")
+      .nextElementSibling.addEventListener("mouseout", function () {
+        if (!flipButton.closest("div").nextElementSibling.hidden) {
+          flipButton.closest("div").hidden = false;
+          flipButton.closest("div").classList.remove("clicked");
+          flipButton.closest("div").nextElementSibling.classList.add("clicked");
+        }
+      });
   }
 }
 
-function setSection() {
-  fillInTheContent();
-  addFlipEffect();
-}
-
-export { setSection, fillInTheContent, addFlipEffect };
+export{ fillInTheContent, addFlipEffect };
 
 // ///// Playing mode
 
